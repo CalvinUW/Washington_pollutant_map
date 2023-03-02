0c0d775c0df3a373ef2151fa52f9181a8401d00d
CalvinUW <calvinst@UW.edu>
2023-03-02 00:41:20 -0800

    Add front page, add front page information, add data information, redo styling of all pages, fix map position error

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 // MAP CODE BELOW ONLY
+let page = document.body.id;
+
+if (page == 'map-page'){
 mapboxgl.accessToken =
     'pk.eyJ1IjoiY2FsdmludXciLCJhIjoiY2xkMjZmczV1MDBsYjNwcDB0cjI4NjFrMCJ9.VQx5_-ESFCLBxyQS4xqiLg';
 const map = new mapboxgl.Map({
@@ -555,5 +558,32 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("main").style.visibility = 'visible';
 }
-
+console.log(page)
 // MAP CODE ABOVE ONLY
+} else if (page == 'index-page'){
+    window.addEventListener('DOMContentLoaded', event => {
+
+        // Activate Bootstrap scrollspy on the main nav element
+        const mainNav = document.body.querySelector('#mainNav');
+        if (mainNav) {
+            new bootstrap.ScrollSpy(document.body, {
+                target: '#mainNav',
+                offset: 74,
+            });
+        };
+    
+        // Collapse responsive navbar when toggler is visible
+        const navbarToggler = document.body.querySelector('.navbar-toggler');
+        const responsiveNavItems = [].slice.call(
+            document.querySelectorAll('#navbarResponsive .nav-link')
+        );
+        responsiveNavItems.map(function (responsiveNavItem) {
+            responsiveNavItem.addEventListener('click', () => {
+                if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                    navbarToggler.click();
+                }
+            });
+        });
+    
+    });
+}
